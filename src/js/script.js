b9532df079b249ddf7ecf16ae37f6eb42f9d3d6b
Brijesh Verma <brijeshverma@cedcoss.com>
2022-09-26 12:22:14 +0530

    JS Objects - Exercise 3

--- src/js/script.js
+++ src/js/script.js
@@ -102,3 +102,11 @@ const person_2 = {
   lastName: "Doe",
   country: "Norway",
 };
+
+// JS Objects - Exercise 3
+// Create an object called person with name = John, age = 50.Then, access the object to alert("John is 50").
+const person_3 = {
+  name: "John",
+  age: 50,
+};
+alert(person_3.name + " is " + person_3.age);
