8f78b7540263f235a3e0f5981bbe893a02a4f7a5
Brijesh Verma <brijeshverma@cedcoss.com>
2022-09-26 11:25:05 +0530

    JS Variables - Exercise 2

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,8 @@
 // JS Variables - Exercise 1
-
 // Create a variable called carName, assign the value Volvo to it.
 var carName = "Volvo";
+
+
+// JS Variables - Exercise 2
+// Create a variable called x, assign the value 50 to it.
+var x = 50;
