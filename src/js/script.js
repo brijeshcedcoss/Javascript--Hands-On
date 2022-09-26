8dc36998b28470262e4eaa09e2d0af9c8c64555f
Brijesh Verma <brijeshverma@cedcoss.com>
2022-09-26 11:32:47 +0530

    JS Variables - Exercise 4

--- src/js/script.js
+++ src/js/script.js
@@ -2,7 +2,6 @@
 // Create a variable called carName, assign the value Volvo to it.
 var carName = "Volvo";
 
-
 // JS Variables - Exercise 2
 // Create a variable called x, assign the value 50 to it.
 var x = 50;
@@ -12,3 +11,10 @@ var x = 50;
 var x = 5;
 var y = 10;
 document.getElementById("demo").innerHTML = x + y;
+
+// JS Variables - Exercise 4
+// Create a variable called z, assign x + y to it, and display the result in an alert box.
+var x = 5;
+var y = 10;
+var z = x + y;
+alert("5 + 10 =" + z);
