d86d8b8af918bac48c04190241e14a8abd5f8a3b
Brijesh Verma <brijeshverma@cedcoss.com>
2022-09-27 13:13:01 +0530

    JS Switch - Exercise 1

--- src/js/script.js
+++ src/js/script.js
@@ -261,3 +261,16 @@ if (x > y) {
 } else {
   alert("Goodbye");
 }
+
+// JS Switch - Exercise 1
+// Create a switch statement that will alert "Hello" if fruits is "banana", and "Welcome" if fruits is "apple".
+
+switch (fruits) {
+  case "Banana":
+    alert("Hello");
+    break;
+
+  case "Apple":
+    alert("Welcome");
+    break;
+}
